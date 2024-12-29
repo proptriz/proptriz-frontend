7d3e4aaadfadad3a6d505d3b029ae74658bc6b12
adisa39 <yusufhammed39@gmail.com>
2024-12-29 05:53:17 +0100

    changed back button to arrow

--- src/components/shared/buttons.tsx
+++ src/components/shared/buttons.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRouter } from "next/navigation";
+import { FaArrowLeft } from "react-icons/fa6";
 import { IoChevronBack } from "react-icons/io5";
 
 
@@ -9,8 +10,8 @@ export const BackButton = ()=> {
     const router = useRouter();
 
     return (
-        <button className="top-5 left-5 p-4 text-xl card-bg rounded-full shadow-md" onClick={()=>router.back()}>
-            <IoChevronBack />
-        </button>   
+        <button className="top-5 left-0 text-xl" onClick={()=>router.back()}>
+            <FaArrowLeft className="text-xl" />
+        </button>
     )
 }

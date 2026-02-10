6698c1a8345c46a7f4179bddfda1cc893956d9e1
haycoder <yusufhammed39@gmail.com>
2026-02-10 15:58:44 +0100

    revamp to horizontal card UI

--- src/components/shared/NavigationTabs.tsx
+++ src/components/shared/NavigationTabs.tsx
@@ -1,19 +1,20 @@
+import { CategoryEnum } from "@/types";
 import React, { SetStateAction }  from "react";
 import { FiHome, FiGrid, FiMap, FiBriefcase, FiMoreHorizontal } from "react-icons/fi";
 
 const tabs = [
-  { name: "House", value:'house', icon: <FiHome /> },
-  { name: "Land", value:'land', icon: < FiMap/> },
-  { name: "Shortlet", value:'shortlet', icon: <FiHome /> },
-  { name: "Hotel", value:'hotel', icon: <FiGrid /> },
-  { name: "Shop", value:'shop', icon: <FiGrid /> },
-  { name: "Office", value:'office', icon: <FiBriefcase /> },
-  { name: "Others", value:'others', icon: <FiMoreHorizontal /> },
+  { name: "House", value:CategoryEnum.house, icon: <FiHome /> },
+  { name: "Land", value:CategoryEnum.land, icon: < FiMap/> },
+  { name: "Shortlet", value:CategoryEnum.shortlet, icon: <FiHome /> },
+  { name: "Hotel", value:CategoryEnum.hotel, icon: <FiGrid /> },
+  { name: "Shop", value:CategoryEnum.shop, icon: <FiGrid /> },
+  { name: "Office", value:CategoryEnum.office, icon: <FiBriefcase /> },
+  { name: "Others", value:CategoryEnum.others, icon: <FiMoreHorizontal /> },
 ];
 
 interface NavigationTabsProps {
   value: string;
-  onChange: React.Dispatch<SetStateAction<string>>;
+  onChange: React.Dispatch<SetStateAction<CategoryEnum>>;
 }
 
 const NavigationTabs: React.FC<NavigationTabsProps> = ({ value, onChange }) => {
